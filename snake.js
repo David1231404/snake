348cbd62516ff2f1cab2fdf9e6a7a389a62c5157
21122023homework <davidkachaanov2004@gmail.com>
2024-08-27 20:50:48 +0300

    first commit

---
+++ snake.js
@@ -0,0 +1,31 @@
+//размеры поля
+var boardWidth = 1280;
+var boardHeight = 800;
+class Snake{
+    constructor(left,top){
+        this.left=Math.round(left);
+        this.top=Math.round(top);
+    }
+render(){
+    let result = document.createElement('div');
+    result.setAttribute('class', 'snake');
+    result.style.left=this.left + 'px';
+    result.style.top=this.top + 'px';
+    return result;
+}
+}
+function addsnake (){
+    let snake=   Snake(
+        Math.random() * boardWidth, 
+        Math.random() * boardHeight 
+);
+    gameBoard.appendChild(
+ snake.render()    
+    );
+}
+
+window.addEventListener(
+
+);
+
+
